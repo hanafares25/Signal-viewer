aec60e8062b1126233466868de7879aec06ed355
Hana Fares <103067954+hanafares25@users.noreply.github.com>
2023-02-24 14:07:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,10 @@ from PIL import Image
 st.set_page_config(page_title= "DSP", page_icon=":ghost:", layout="wide")
 
 
-#image = Image.open( "C:\Users\hanaf\Pictures\Screenshots\Screenshot (10).png")
-# ------ HEADER SECTION ---------
-with st.container():
-    st.subheader("Hi, I am Hana :wave:")
+
     st.title("Digital Signal Processing")
     st.write("Signal viewer Task 1")
-    st.write("[Task 1 >](https://github.com/dina-khalid/DSP_Task1_-13-)")
+    st.write("[Task 1 >](https://github.com/hanafares25/Signal-viewer.git)")
 
 
 
@@ -19,31 +16,4 @@ with st.container():
     st.write("---")
     left_column, right_column= st.columns(2)
     with left_column:
-        st.header(" What we need to do")
-        st.write("##")
-        st.write(""" Develop a web application that illustrates multi-channel signal viewer
-        The user can browse his PC to open any signal file. Each group will need to provide samples from three different 
-medical signals (e.g. ECG, EMG, EEG,…etc). Each signal type should have an example for normal signal and abnormal 
-signal.
-- Your application should contain two main identical graphs. The user can open different signals in each graph. i.e. each 
-graph has to have its own controls. The user can run each graph independently or link both graphs via a button in the 
-UI. When the graphs are linked, the two graphs must display the same time frames, signal speed, and same viewport if 
-zoomed or panned (i.e. if the user zoom on one graph, the other graph should apply the same exact zoom as well). If 
-the link of the two graphs is disabled, then each graph can run its signals independently.
-- In any of the two graphs, when the user opens a signal file, the signal should show up in the cine mode (i.e. a running 
-signal through time, similar to the one you see in the ICU monitors). Do NOT open a signal in a static mode. If the 
-signal ends, there should be a rewind option to either stop the signal or start running it again from the beginning.
-- The use can manipulate the running signals through UI elements that provide the below function:
-• Change color,
-• Add a label/title for each signal,
-• Show/hide,
-• Control/customize the cine speed,
-• Zoom in/out,
-• Pause/play/rewind(on/off),
-• Scroll/Pan the signal in any direction (left, top, right, bottom). Scroll is performed through sliders, and pan is
-performed through the mouse movements.
- 
-        """)
-
-        #with right_column:
-            #st.image(image )
+        
